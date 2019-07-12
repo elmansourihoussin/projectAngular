00d655e7faee1068c26424a9a27ab3fbe8209b3c
houssin mansouri <houssin.mansouri93@gmail.com>
2019-07-12 16:19:22 +0000

    nouvelle modification

---
+++ src/app/main/content/education/education.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EducationComponent } from './education.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EducationComponent]
+})
+export class EducationModule { }
